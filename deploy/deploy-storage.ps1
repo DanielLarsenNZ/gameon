152e4dc3bc7fadf79e269d779319412e5c128969
Daniel Larsen <dalars@microsoft.com>
2021-02-28 21:42:58 +1300

    Custom apex domain + SSL cert on Front Door

--- deploy/deploy-storage.ps1
+++ deploy/deploy-storage.ps1
@@ -3,6 +3,10 @@ $ErrorActionPreference = 'Stop'
 . ./vars.ps1
 
 $frontDoor = 'gameonnz'
+$frontDoorFrontEnd = 'gameonnz'
+$ttl = 300
+$kvSecretName = 'GameOnNZCSR2'
+$kvSecretVersion = '3e2dc2ac68f24800a354cf56ccba1795'
 
 # STORAGE ACCOUNT
 #az extension add --name storage-preview
@@ -18,19 +22,36 @@ $spaUrl
 # FRONT DOOR
 #az extension add -n front-door
 
-az network front-door create -n $frontDoor -g $rg --tags $tags `
-    --frontend-host-name $domainName `
-    --backend-address $spaUrl `
-    --accepted-protocols Http Https `
-    --protocol Http
+# $frontDoorId = ( az network front-door create -n $frontDoor -g $rg --tags $tags `
+#     --backend-address $spaUrl `
+#     --accepted-protocols Http Https `
+#     --protocol Http | ConvertFrom-Json ).id
 
-az network front-door frontend-endpoint enable-https --front-door-name $frontDoor `
-    --name 'DefaultFrontendEndpoint' `
-    --resource-group $rg
-    -certificate-source 'FrontDoor' `
-    --minimum-tls-version 1.2
+$frontDoorId = ( az network front-door create -n $frontDoor -g $rg --tags $tags --accepted-protocols Http Https --backend-address $spaUrl | ConvertFrom-Json ).id
 
+
+# Apex hostname on gameon.nz
+# Create an Alias DNS recordset
+az network dns record-set a create -n "@" -g $rg --zone-name $domainName --if-none-match --target-resource $frontDoorId --ttl $ttl
+
+# Create the domain verifiy CNAME
+az network dns record-set cname set-record -g $rg --zone-name $domainName --if-none-match --record-set-name "afdverify.$domainName" --cname "afdverify.$frontDoor.azurefd.net" --ttl $ttl
+
+# Create a frontend for the custom domain
+az network front-door frontend-endpoint create --front-door-name $frontDoor --host-name $domainName --name $frontDoorFrontEnd -g $rg --session-affinity-enabled 'Disabled'
+
+# Enable HTTPS, Front Door managed cert is not possible currently 😢 So have to have to upload cert to Key Vault
+# Follow the steps in this article to give Front Door access to KV: https://docs.microsoft.com/en-us/azure/frontdoor/front-door-custom-domain-https#option-2-use-your-own-certificate
+
+# get KV id
+$kvId = ( az keyvault show -n $kv -g $rg | ConvertFrom-Json ).id
+az network front-door frontend-endpoint enable-https --front-door-name $frontDoor --name $frontDoorFrontEnd -g $rg --certificate-source 'AzureKeyVault' `
+    --vault-id $kvId --secret-name $kvSecretName --secret-version $kvSecretVersion
+
+# Update the default routing rule to include the new frontend
 az network front-door routing-rule update --front-door-name $frontDoor -n 'DefaultRoutingRule' -g $rg `
-    --caching 'Enabled'
+    --caching 'Enabled' --frontend-endpoints 'DefaultFrontendEndpoint' $frontDoorFrontEnd
 
 start "https://$frontDoor.azurefd.net"
+start "http://$domainName"
+start "https://$domainName"
