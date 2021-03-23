3b8600c8beb6b7491653489b6f3ce81bbacdaa8e
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-03-23 16:04:14 +1000

    Added Dev Portal

---
+++ client/src/components/DevToken/index.js
@@ -0,0 +1,80 @@
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useAccount, useMsal } from '@azure/msal-react';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Input, Row } from 'reactstrap';
+const { REACT_APP_AAD_CLIENT_ID } = process.env;
+
+const DevToken = () => {
+  const { instance, accounts } = useMsal();
+  const account = useAccount(accounts[0] || {});
+  const [data, setData] = useState({ message: 'You are not authenticated.' });
+
+  const formatResponse = (response) => {
+    return {
+      access_token: response.accessToken,
+      expires_on: response.expiresOn,
+      name: response.account.name,
+      tenantId: response.account.tenantId,
+      username: response.account.username,
+      scopes: response.scopes,
+    };
+  };
+
+  const copyToClipboard = (e) => {
+    navigator.clipboard.writeText(data.access_token);
+  };
+
+  useEffect(() => {
+    if (account) {
+      instance
+        .acquireTokenSilent({
+          scopes: [`${REACT_APP_AAD_CLIENT_ID}/Users`],
+          account: account,
+        })
+        .then((response) => {
+          if (response) {
+            setData(formatResponse(response));
+          }
+        })
+        .catch(function (error) {
+          // TODO: Acquire token silent failure, and send an interactive request
+          console.log(error);
+          if (error.errorMessage.indexOf('interaction_required') !== -1) {
+            instance.acquireTokenRedirect({
+              scopes: ['api://GameOn.Api/Users'],
+              account: account,
+            });
+          }
+        });
+    }
+  }, [account, instance]);
+
+  return (
+    <>
+      <Row className="page-title">
+        <Col md={3} xl={6}>
+          <h4 className="mb-1 mt-0">Developer Access Token</h4>
+        </Col>
+      </Row>
+
+      <AuthenticatedTemplate>
+        <Row>
+          <Col>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </Col>
+        </Row>
+        <Button onClick={() => copyToClipboard()}>Copy Access Token</Button>
+      </AuthenticatedTemplate>
+
+      <UnauthenticatedTemplate>
+        <Row>
+          <Col md={3} xl={6}>
+            <p className="mb-1 mt-0">Please sign in to view access token</p>
+          </Col>
+        </Row>
+        <Button onClick={() => instance.loginRedirect()}>Sign In</Button>
+      </UnauthenticatedTemplate>
+    </>
+  );
+};
+
+export default DevToken;
