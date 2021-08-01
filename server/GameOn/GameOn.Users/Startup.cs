70eb1354add4efdced8f342db12e628a5c4aab3c
Noah Costar <n.f.costar@gmail.com>
2021-08-02 10:25:45 +1200

    Adding Swashbuckle to remaining projects

--- server/GameOn/GameOn.Users/Startup.cs
+++ server/GameOn/GameOn.Users/Startup.cs
@@ -34,6 +34,9 @@ namespace GameOn.Users
 
             services.AddTransient<GraphService>();
             services.AddTransient<UsersService>();
+            
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
 
             services
                 .AddControllers()
@@ -49,7 +52,17 @@ namespace GameOn.Users
                 app.UseDeveloperExceptionPage();
                 Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
             }
+            
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(); // TODO: check order with @Dan
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GameOn API V1");
+            });
+            
             // Middleware order is crucial! https://docs.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-3.1
             app.UseRouting();
             app.UseAuthentication();
